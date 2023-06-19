3c3d9fb7b39d3f57974d37dec7dc2a4eeb41bb54
jacob9301 <jacob.luc.bushby@gmail.com>
2023-06-19 16:43:14 +0100

    modified:   app/src/app/page.tsx 	modified:   app/src/components/navigation/Navbar.tsx

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,21 +1,9 @@
+import Navbar from '../components/navigation/Navbar'
+
 export default function Home() {
   return (
     <div>
-      <nav className="flex items-center justify-between py-8">
-        <h1 className="text-3xl text-[#08251C] font-bold font-sans">one day</h1>
-        <div className="hidden lg:flex ml-auto mr-14 gap-12">
-          <span className="text-xl tracking-wide">Mindfulness Journal</span>
-          <span className="text-xl tracking-wide">Mood Tracker</span>
-        </div>
-        <div className="lg:hidden items-center h-11 p-3 ml-auto mr-4 space-y-2 bg-gray-500 rounded shadow">
-          <span className="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
-          <span className="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
-          <span className="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
-        </div>
-        <div className="flex items-center py-2 h-11 tracking-wide font-serif font-bold text-xl rounded px-10 text-white bg-[#9AB8AE]">
-          Login
-        </div>
-      </nav>
+      <Navbar />
       <main className="">
         <section className="grid lg:grid-cols-7 gap-14">
           <div className="flex flex-col lg:col-span-3 bg-[#384B42] p-12 rounded-2xl">
--- app/src/components/navigation/Navbar.tsx
+++ app/src/components/navigation/Navbar.tsx
@@ -1,7 +1,21 @@
 const Navbar = () => {
     return (
-        <div>
-            hi
+        <nav className="flex items-center justify-between py-8">
+        <h1 className="text-3xl text-[#08251C] font-bold font-sans">one day</h1>
+        <div className="hidden lg:flex ml-auto mr-14 gap-12">
+          <span className="text-xl tracking-wide">Mindfulness Journal</span>
+          <span className="text-xl tracking-wide">Mood Tracker</span>
         </div>
+        <div className="lg:hidden items-center h-11 p-3 ml-auto mr-4 space-y-2 bg-gray-500 rounded shadow">
+          <span className="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
+          <span className="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
+          <span className="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
+        </div>
+        <div className="flex items-center py-2 h-11 tracking-wide font-serif font-bold text-xl rounded px-10 text-white bg-[#9AB8AE]">
+          Login
+        </div>
+      </nav>
     )
 }
+
+export default Navbar;
