1840ce4913df7624c97af25295068165b8bcfc9d
jacob9301 <jacob.luc.bushby@gmail.com>
2023-07-03 23:53:21 +0100

    modified:   app/package-lock.json 	modified:   app/package.json 	modified:   app/src/components/forms/LoginForm.tsx 	modified:   app/src/components/forms/SignupForm.tsx 	modified:   app/src/components/input/Dropdown.tsx

--- app/src/components/forms/LoginForm.tsx
+++ app/src/components/forms/LoginForm.tsx
@@ -1,24 +1,40 @@
 "use client";
 
 import Link from 'next/link'
-import { useState } from "react";
+import { Formik, Form, Field, FormikHelpers } from 'formik';
+
+interface Values {
+    username: string;
+    password: string;
+}
+
+const handleSubmit = (values: Values, { setSubmitting }: FormikHelpers<Values>) => {
+    setTimeout(() => {
+        alert(JSON.stringify(values));
+        setSubmitting(false);
+    }, 400)
+}
 
 const LoginForm = () => {
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-    }
-
     return (
         <div className='flex flex-col justify-center'>
-            <form className='flex flex-col justify-center' onSubmit={handleSubmit}>
-                <h1 className="text-4xl lg:text-6xl leading-normal tracking-wide font-bold mb-6 text-gray-800">Login</h1>
-                <label  htmlFor="inputUsername">Username</label>
-                <input type='text' id='inputUsername' name='username' />
-                <label htmlFor="inputPassword">Password</label>
-                <input type='text' id='inputPassword' name='password' />
-                <button className='text-xl w-350px h-63px bg-[#00A870] py-4 px-8 text-white font-serif font-bold rounded' type='submit'>Login</button>
-            </form>
+            <h1 className="text-center text-4xl lg:text-6xl leading-normal tracking-wide font-bold mb-6 text-gray-800">Login</h1>
+            <Formik
+                className='flex flex-col justify-center'
+                initialValues={{ username: '', password: '' }}
+                onSubmit={handleSubmit}
+            >
+                <Form className='flex flex-col justify-center'>
+                    <label  htmlFor="username">Username</label>
+                    <Field type='text' id='username' name='username' />
+
+                    <label htmlFor="password">Password</label>
+                    <Field type='password' id='password' name='password' />
+
+                    <button className='text-xl w-350px h-63px bg-[#00A870] py-4 px-8 text-white font-serif font-bold rounded' >Login</button>
+                </Form>
+            </Formik>
             <div>
                 Don't have an account? <Link className='text-[#6CA390]' href='/signup'>Sign up here</Link>
             </div>
--- app/src/components/forms/SignupForm.tsx
+++ app/src/components/forms/SignupForm.tsx
@@ -1,30 +1,54 @@
 "use client";
 
 import Link from 'next/link'
-import { useState } from "react";
+import { Formik, Form, Field, FormikHelpers } from 'formik';
+interface Values {
+    username: string;
+    password: string;
+    passwordConfirm: string;
+    accept: boolean;
+}
+
+const handleSubmit = (values: Values, { setSubmitting }: FormikHelpers<Values>) => {
+    setTimeout(() => {
+        alert(JSON.stringify(values));
+        setSubmitting(false);
+    }, 400)
+}
 
 const SignupForm = () => {
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-    }
-
     return (
         <div className='flex flex-col justify-center'>
-            <form className='flex flex-col justify-center' onSubmit={handleSubmit}>
-                <h1 className="text-4xl lg:text-6xl leading-normal tracking-wide font-bold mb-6 text-gray-800">Sign Up</h1>
-                <label  htmlFor="inputUsername">Username</label>
-                <input type='text' id='inputUsername' name='username' />
-                <label htmlFor="inputPassword">Password</label>
-                <input type='text' id='inputPassword' name='password' />
-                <label htmlFor="inputPassword">Confirm Password</label>
-                <input type='text' id='confirmPassword' name='passwordConf' />
-                <label className="flex items-center">
-                    <input type='checkbox' className="mr-2" />
-                    <span>I accept the terms and privacy policy</span>
-                </label>
-                <button className='text-xl w-350px h-63px bg-[#00A870] py-4 px-8 text-white font-serif font-bold rounded' type='submit'>Login</button>
-            </form>
+            <h1 className="text-center text-4xl lg:text-6xl leading-normal tracking-wide font-bold mb-6 text-gray-800">Sign Up</h1>
+            <Formik
+                className='flex flex-col justify-center'
+                initialValues={{ 
+                    username: '', 
+                    password: '', 
+                    passwordConfirm: '', 
+                    accept: false 
+                }}
+                onSubmit={handleSubmit}
+            >
+                <Form className='flex flex-col justify-center'>
+                    <label  htmlFor="username">Username</label>
+                    <Field type='text' id='username' name='username' />
+
+                    <label htmlFor="password">Password</label>
+                    <Field type='password' id='password' name='password' />
+
+                    <label htmlFor="passwordConfirm">Confirm password</label>
+                    <Field type='password' id='passwordConfirm' name='passwordConfirm' />
+
+                    <label className="flex items-center">
+                        <Field type='checkbox' name='accept' className="mr-2" />
+	                    <span>I accept the terms and privacy policy</span>
+                    </label>
+
+                    <button className='text-xl w-350px h-63px bg-[#00A870] py-4 px-8 text-white font-serif font-bold rounded' >Sign up</button>
+                </Form>
+            </Formik>
             <div>
                 Already have an account? <Link className='text-[#6CA390]' href='/login'>Login here</Link>
             </div>
